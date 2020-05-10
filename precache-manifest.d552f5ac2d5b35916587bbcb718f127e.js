ef112ed94288cdf54babd4500cea3c6c3b2834a9
Edwin Torres <codesandtags@gmail.com>
2020-05-10 13:32:22 -0500

    Updates

---
+++ precache-manifest.d552f5ac2d5b35916587bbcb718f127e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a7c7698e4dc0ad15fcde4929bee362",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "c70522ad7e8adb909aca",
+    "url": "/face-recognition-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dafd8eca56234d84f1ed",
+    "url": "/face-recognition-brain/static/css/main.1f9c01b9.chunk.css"
+  },
+  {
+    "revision": "c70522ad7e8adb909aca",
+    "url": "/face-recognition-brain/static/js/2.7a0848c9.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition-brain/static/js/2.7a0848c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafd8eca56234d84f1ed",
+    "url": "/face-recognition-brain/static/js/main.4395dc06.chunk.js"
+  },
+  {
+    "revision": "29e5821f791c973dcb04",
+    "url": "/face-recognition-brain/static/js/runtime-main.e3020e8e.js"
+  }
+]);
